3d860f346bcf9039d43c15e55412aed10c606902
Zahaawii <zaak@kromannreumert.com>
2024-10-27 17:42:18 +0100

    added comments to the project

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,9 @@ import Userinterface.UI;
 
 public class Main {
 
-
     public static void main(String[] args) {
+    //Method created to give our main as little code as possible.
     UI.menu();
-
     }
 }
 
